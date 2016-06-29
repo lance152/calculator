ccfbc4c103066869126166be5455579e053118f9
Fan Lance <FanLance@fan-lances-macbook-pro.local>
2016-06-29 21:09:37 +0800

    a simple working calculator

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -12,10 +12,66 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
+    var isUserInMiddleOfTyping = false
     
-    @IBAction func append(sender: UIButton) {
-        let digit = sender.currentTitle!
-        display.text = display.text! + digit
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isUserInMiddleOfTyping{
+            enter()
+        }
+        switch operation {
+        case "×": performOperation{$0 * $1}
+        case "÷": performOperation{$1 / $0}
+        case "+": performOperation{$1 + $0}
+        case "−": performOperation{$1 - $0}
+        case "√": performOperation{sqrt($0)}
+        default:break
+        }
     }
+    
+    func performOperation (operation: (Double,Double) -> Double){
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation (operation: Double-> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if isUserInMiddleOfTyping{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            isUserInMiddleOfTyping=true
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        isUserInMiddleOfTyping=false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set{
+            display.text = "\(newValue)"
+            isUserInMiddleOfTyping=false
+        }
+    }
+    
 }
 
